b178f1b48d2660eb0e641638b344f959cc89f30a
Bruno José <bru.matias100@gmail.com>
2025-12-07 21:03:49 -0300

    feat: creating new room

--- src/components/create-room-form.tsx
+++ src/components/create-room-form.tsx
@@ -19,6 +19,7 @@ import {
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
 import { Textarea } from '@/components/ui/textarea';
+import { useCreateRoom } from '@/http/use-create-room';
 
 const createRoomSchema = z.object({
   name: z.string().min(3, 'Inclua no mínimo 3 caracteres'),
@@ -28,6 +29,8 @@ const createRoomSchema = z.object({
 type CreateRoomFormData = z.infer<typeof createRoomSchema>;
 
 export function CreateRoomForm() {
+  const { mutateAsync: createRoom } = useCreateRoom();
+
   const createRoomForm = useForm<CreateRoomFormData>({
     resolver: zodResolver(createRoomSchema),
     defaultValues: {
@@ -36,8 +39,9 @@ export function CreateRoomForm() {
     },
   });
 
-  function handleCreateRoom(data: CreateRoomFormData) {
-    console.log(data);
+  async function handleCreateRoom({ name, description }: CreateRoomFormData) {
+    await createRoom({ name, description });
+    createRoomForm.reset();
   }
 
   return (
